8a2092abf2d1d881dcfcaf3ec9a6fce9181ba40a
somudas <soumyadeep4122002@gmail.com>
2022-05-26 17:47:33 +0530

    LIS using C++

---
+++ c++/LIS.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define N (int)1e6+10
+
+int dp[N];
+int numbers[N];
+
+int lis(int index){
+    if (dp[index] != 0) return dp[index];
+    int len = 1;
+    for(int i = index-1;i>=0;i--){
+    	if(numbers[i]<numbers[index])
+        	len = max(len, lis(i)+1);
+    }
+    return dp[index]=len;
+}
+
+
+int main(void){
+
+    int length;
+    cin >> length;    
+
+    for(int i=0;i<length;i++){
+        cin >> numbers[i];
+    }
+
+    int max_length = 1;
+    for(int i=0;i<length;i++){
+    	max_length = max(max_length, lis(i)); 	
+    } 
+    
+    cout << max_length << endl;
+
+    return 0;
+}
